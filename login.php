790b812b874dd066ce2461343ab58e5ea27a4a24
shantokarmoker8 <shantokarmoker8@gmail.com>
2026-01-28 14:06:39 +0600

    login page

---
+++ login.php
@@ -0,0 +1,33 @@
+<?php
+    include("connection.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+    <div class="form">
+        <form action="" method="post">
+            <h2>Login</h2>
+            <p class="msg"></p>
+            
+            <div class="form-group">
+                <input type="email" name="email" id="" placeholder="Enter Your Email" class="form-control" require>
+            </div>
+        
+            <div class="form-group">
+                <input type="password" name="password" id="" placeholder="Enter Your Password" class="form-control" require>
+            </div>
+           
+            <button name="submit" class="btn btn-primary">Login Now</button>
+            <p>Don't have an Account? <a href="register.php">Registration Now</a></p>
+        </form>
+    </div>
+</body>
+</html>
