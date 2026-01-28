18b324532a2a53780c63fb81ef47783e21882e58
shantokarmoker8 <shantokarmoker8@gmail.com>
2026-01-28 13:38:33 +0600

    database connection

---
+++ connection.php
@@ -0,0 +1,16 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "login_and_register";
+
+    $con = mysqli_connect($servername, $username, $password, $database);
+
+    if(!($con)){
+        echo "Connection faild";
+    }
+    else{
+        echo "connection successfull";
+    }
+
+?>
